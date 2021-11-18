6503783b0c6ae36c0f07a895f683fdb48f6d4596
Christian <christianccm17@gmail.com>
2021-11-18 11:39:33 -0600

    Mas cambios, y reportes en Funcionarios

--- cliente/src/app/componentes/funcionarios/funcionarios.component.ts
+++ cliente/src/app/componentes/funcionarios/funcionarios.component.ts
@@ -1,8 +1,18 @@
 import { IRespuesta } from 'src/app/modelo/Respuesta.models';
 import { SexoService } from 'src/app/servicios/sexoService';
 import { DepartamentoService } from './../../servicios/departamento.service';
-import {Component,OnInit,ViewChild,ElementRef, OnDestroy} from '@angular/core';
-import { Funcionario1, Funcionario ,FuncionarioD } from 'src/app/modelo/Funcionario.model';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  OnDestroy,
+} from '@angular/core';
+import {
+  Funcionario1,
+  Funcionario,
+  FuncionarioD,
+} from 'src/app/modelo/Funcionario.model';
 import { FuncionarioService } from './../../servicios/funcionario.service';
 import { NgForm } from '@angular/forms';
 import Swal from 'sweetalert2';
@@ -14,60 +24,74 @@ import { Swal2 } from './../../mensajes/mensajes';
   templateUrl: './funcionarios.component.html',
   styleUrls: ['./funcionarios.component.css'],
 })
-export class FuncionariosComponent implements OnInit,OnDestroy {
+export class FuncionariosComponent implements OnInit, OnDestroy {
   @ViewChild(DataTableDirective, { static: false })
   dtElement: DataTableDirective;
   funcionario: Funcionario = {
-    idFuncionario:"",
-    idDepartamento: "",
-    idSexo: "",
-    nombre: "",
-    apellidos: "",
-    fachaNacimiento: "",
-    foto: "",
-    loginName: "",
-    password: "",
+    idFuncionario: '',
+    idDepartamento: '',
+    idSexo: '',
+    nombre: '',
+    apellidos: '',
+    fachaNacimiento: '',
+    foto: '',
+    loginName: '',
+    password: '',
   };
   funcionarioD: FuncionarioD = {
-    departamento: "",
-    sexo: "",
-    nombre: "",
-    apellidos: "",
-    fachaNacimiento: "",
-    foto: "",
-    loginName: "",
+    departamento: '',
+    sexo: '',
+    nombre: '',
+    apellidos: '',
+    fachaNacimiento: '',
+    foto: '',
+    loginName: '',
   };
 
   @ViewChild('funcionarioForm') funcionarioForm: NgForm;
   @ViewChild('botonCerrar') botonCerrar: ElementRef;
   @ViewChild('botonAbrir') botonAbrir: ElementRef;
   @ViewChild('botonDetalles') botonDetalles: ElementRef;
-  
 
-  dtOptions: DataTables.Settings = {};
+  dtOptions: any = {};
   funcionarios!: any[];
   tablaTrigger = new Subject<any>();
   departamentos!: any[];
   sexo!: any[];
-  habilidado:boolean = false;
+  habilidado: boolean = false;
+  fotoValida: boolean = true;
   constructor(
     private funcionarioService: FuncionarioService,
     private departamentoService: DepartamentoService,
     private sexoService: SexoService,
-    private swal:Swal2
+    private swal: Swal2
   ) {}
 
   onFileChanged(e: any) {
     console.log(e[0].base64);
-    this.funcionario.foto = e[0].base64;
+    let extencionPermitida = /(.png|.jpg)$/i;
+    let extencionvalida = e[0].name;
+    if (!extencionPermitida.exec(extencionvalida)) {
+      this.swal.error('Solo se permiten Fotos (.png y .jpg)');
+      return;
+    } else {
+      e[0].base64 ? (this.fotoValida = true) : (this.fotoValida = false);
+      this.funcionario.foto = e[0].base64;
+    }
   }
+  onFileData(e: any) {
+    console.log(this.fotoValida);
+    this.fotoValida = false;
+    console.log(this.fotoValida);
+  }
+
   ngOnInit(): void {
     this.obtenerFuncionarios();
     this.obtenerDepartamentos();
     this.obtenerSexo();
   }
 
-  ngOnDestroy(): void{
+  ngOnDestroy(): void {
     this.tablaTrigger.unsubscribe();
   }
   obtenerDepartamentos() {
@@ -81,13 +105,43 @@ export class FuncionariosComponent implements OnInit,OnDestroy {
     });
   }
   obtenerFuncionarios() {
-    this.dtOptions = {
+    this.dtOptions = { 
       pagingType: 'full_numbers',
       pageLength: 6,
+      responsive: true,
+     // dom: '<lf<Bt>ip>',
       language: {
         url: '//cdn.datatables.net/plug-ins/1.11.3/i18n/es_es.json',
       },
+      processing: true,
+      dom: 'Bfrtip',
+      buttons: [
+        {
+          extend: 'excelHtml5', 
+          titleAttr: 'Exportar a Excel',
+          title:"Reporte Funcionarios",
+          text:'<i class="fas fa-file-excel fs-4"></i>',
+          className: 'bg-success text-light rounded-3',
+          filename: 'Reporte de Funcionarios',
+          exportOptions: {
+            columns: [0, 1, 2, 3, 4],
+          },
+        },
+        {
+          extend: 'pdfHtml5',
+          titleAttr: 'Exportar a PDF',
+          title:"Reporte Funcionarios",
+          text:'<i class="fas fa-file-pdf fs-4"></i>',
+          className: 'bg-danger text-light rounded-3',
+          filename: 'Reporte de Funcionarios',
+          exportOptions: {
+            columns: [0, 1, 2, 3, 4],
+          },
+          
+        },
+      ],
     };
+
     this.funcionarioService.getAllFuncionarios().subscribe((funcionarios) => {
       this.funcionarios = funcionarios;
       console.log(funcionarios);
@@ -107,9 +161,10 @@ export class FuncionariosComponent implements OnInit,OnDestroy {
         });
     });
   }
-  verDetalles(funcionario:Funcionario){
-    this.funcionarioService.getByIdDetallesFuncionario(funcionario).subscribe(
-      (result)=>{
+  verDetalles(funcionario: Funcionario) {
+    this.funcionarioService
+      .getByIdDetallesFuncionario(funcionario)
+      .subscribe((result) => {
         console.log(result[0].nombre);
         this.funcionarioD.departamento = result[0].departamento;
         this.funcionarioD.sexo = result[0].sexo;
@@ -118,80 +173,74 @@ export class FuncionariosComponent implements OnInit,OnDestroy {
         this.funcionarioD.loginName = result[0].loginName;
         this.funcionarioD.foto = result[0].foto;
         this.funcionarioD.fachaNacimiento = result[0].fechaNacimiento;
-      }
-    )
-   this.botonDetalles.nativeElement.click();
+      });
+    this.botonDetalles.nativeElement.click();
   }
-  cerrarDetalles(){
-    this.funcionario.idFuncionario = "";
-        this.funcionarioD.sexo = "";
-        this.funcionarioD.nombre = "";
-        this.funcionarioD.apellidos = "";
-        this.funcionarioD.loginName ="";
-        this.funcionarioD.foto = "";
-        this.funcionarioD.departamento = "";
-        this.funcionarioD.fachaNacimiento = "";
-        this.habilidado = false;
+  cerrarDetalles() {
+    this.funcionario.idFuncionario = '';
+    this.funcionarioD.sexo = '';
+    this.funcionarioD.nombre = '';
+    this.funcionarioD.apellidos = '';
+    this.funcionarioD.loginName = '';
+    this.funcionarioD.foto = '';
+    this.funcionarioD.departamento = '';
+    this.funcionarioD.fachaNacimiento = '';
+    this.habilidado = false;
   }
   editarFuncionario(funcionario: Funcionario) {
     console.log(funcionario);
-      this.funcionarioService.getById(funcionario).subscribe(
-        (result)=>{
-          console.log("Editar");
-          console.log(result[0].nombre);
-          console.log("Editar");
-          this.funcionario.idFuncionario = result[0].idFuncionario;
-          this.funcionario.idSexo = result[0].idSexo;
-          this.funcionario.nombre = result[0].nombre;
-          this.funcionario.apellidos = result[0].apellidos;
-          this.funcionario.loginName = result[0].loginName;
-          this.funcionario.foto = result[0].foto;
-          this.funcionario.idDepartamento = result[0].idDepartamento;
-          this.funcionario.fachaNacimiento = result[0].fechaNacimiento;
-          this.funcionario.password = "*****";
-          this.habilidado = true;
-          this.abrirModal();
-         
-        }
-      )
+    this.funcionarioService.getById(funcionario).subscribe((result) => {
+      console.log('Editar');
+      console.log(result[0].nombre);
+      console.log('Editar');
+      this.funcionario.idFuncionario = result[0].idFuncionario;
+      this.funcionario.idSexo = result[0].idSexo;
+      this.funcionario.nombre = result[0].nombre;
+      this.funcionario.apellidos = result[0].apellidos;
+      this.funcionario.loginName = result[0].loginName;
+      this.funcionario.foto = result[0].foto;
+      this.funcionario.idDepartamento = result[0].idDepartamento;
+      this.funcionario.fachaNacimiento = result[0].fechaNacimiento;
+      this.funcionario.password = '*****';
+      this.habilidado = true;
+      this.abrirModal();
+    });
   }
 
   agregar(funcionarioForm: NgForm) {
     console.log(this.funcionarioForm.value);
     let msg;
-    console.log("ID FUNCIONARIO "+this.funcionario.idFuncionario);
+    console.log('ID FUNCIONARIO ' + this.funcionario.idFuncionario);
     console.log(this.funcionario);
     if (funcionarioForm.valid) {
-       
-      if(this.funcionario.idFuncionario  =='' || this.funcionario.idFuncionario == null){
-       this.funcionarioService.nuevoFuncionario(this.funcionario).subscribe(
-         (result)=>{
-           console.log(result);
-           this.reload();
-         }
-       )
-       msg="Agregado correctamente";
-       
-      }else{
-        console.log("En Editar");
-        this.funcionarioService.updateFuncionario(this.funcionario).subscribe(
-          (result)=>{
-                 console.log(result);
-                 this.reload();
-            }
-        )
-        msg="Actualizado correctamente";
+      if (
+        this.funcionario.idFuncionario == '' ||
+        this.funcionario.idFuncionario == null
+      ) {
+        this.funcionarioService
+          .nuevoFuncionario(this.funcionario)
+          .subscribe((result) => {
+            console.log(result);
+            this.reload();
+          });
+        msg = 'Agregado correctamente';
+      } else {
+        console.log('En Editar');
+        this.funcionarioService
+          .updateFuncionario(this.funcionario)
+          .subscribe((result) => {
+            console.log(result);
+            this.reload();
+          });
+        msg = 'Actualizado correctamente';
       }
 
-     this.swal.exitoso(msg);
-     this.cerrarModal();
-     
-    } 
-
+      this.swal.exitoso(msg);
+      this.cerrarModal();
+    }
   }
 
   eliminarFuncionario(funcionario1: Funcionario1) {
-
     const { nombre, apellidos, departamento } = funcionario1;
     Swal.fire({
       title: 'Estas Seguro de Eliminar el Siguiente Funcionario?',
@@ -203,33 +252,27 @@ export class FuncionariosComponent implements OnInit,OnDestroy {
       confirmButtonText: 'Si, eliminar!',
     }).then((result) => {
       if (result.isConfirmed) {
-
-        this.funcionarioService.delete(funcionario1).subscribe(
-          (result)=>{
-            let data = result as IRespuesta
-            console.log(data);
-           if(data.valido='Si'){
-             this.swal.exitoso(data.msg);
-             this.reload();
-           
-           }else{
-             this.swal.error(data.msg);
-           }
-            
+        this.funcionarioService.delete(funcionario1).subscribe((result) => {
+          let data = result as IRespuesta;
+          console.log(data);
+          if ((data.valido = 'Si')) {
+            this.swal.exitoso(data.msg);
+            this.reload();
+          } else {
+            this.swal.error(data.msg);
           }
-        )
-       
+        });
       }
     });
     return;
   }
-   cerrarModal() {
+  cerrarModal() {
     this.botonCerrar.nativeElement.click();
     this.funcionarioForm.resetForm();
-   // this.funcionario.foto = '';
+    // this.funcionario.foto = '';
     this.habilidado = false;
   }
-  private abrirModal(){
+  private abrirModal() {
     this.botonAbrir.nativeElement.click();
   }
 }
--- cliente/src/app/modelo/Avance.model.ts
+++ cliente/src/app/modelo/Avance.model.ts
@@ -14,5 +14,6 @@ export interface Avance{
     descripcion?:string;
     nombre?:string;
     apellidos?:string;
+    nombreSolicitud?:string;
 
  }
