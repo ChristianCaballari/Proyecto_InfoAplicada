54fd697d243e58b4ef0a666dea414497db6b38f1
Abigail González <pamelagon12@gmail.com>
2021-12-11 01:01:48 -0600

    Cambios finales

--- cliente/src/app/componentes/funcionarios/funcionarios.component.ts
+++ cliente/src/app/componentes/funcionarios/funcionarios.component.ts
@@ -165,6 +165,9 @@ export class FuncionariosComponent implements OnInit, OnDestroy {
           exportOptions: {
             columns: [0, 1, 2, 3, 4],
           },
+          customize: function(doc) {
+            doc.content[1].margin = [ 70, 0, 100, 0 ] //left, top, right, bottom
+          }
           
         },
       ],
