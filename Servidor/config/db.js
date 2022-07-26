008ca9df032d43f876d88f2d5f7cba5f2fc2392e
Abigail González <pamelagon12@gmail.com>
2022-07-25 22:54:36 -0600

    Backup BD

--- Servidor/config/db.js
+++ Servidor/config/db.js
@@ -1,15 +1,15 @@
 const sql = require('mssql')
 const config={
-    server: 'localhost',
-    user:'logChristian',
-    password:'123',
+    // server: 'localhost',
+    // user:'logChristian',
+    // password:'123',
     
     
     database:'proyectoInfoAplicada',
    
-        //    server: 'DESKTOP-093HK3B\\SQLEXPRESS',
-        //    user:'root',
-        //    password:'12345',
+           server: 'DESKTOP-093HK3B\\SQLEXPRESS',
+           user:'root',
+           password:'12345',
     
     options: {
         trustServerCertificate: true,
